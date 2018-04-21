8c485254b854e95303524fb49f2fc74750f83c87
cusengan <lordvinh1@gmail.com>
2018-04-21 14:25:38 -0500

    Configuration complete

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,21 @@
 require('./config/config');
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('./db/mongoose');
+
 const app = express();
 
 const port = process.env.PORT;
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.send('we own');
+});
+
+app.listen(port, ()=>{
+  console.log(`app is listening on port ${port}`);
+});
