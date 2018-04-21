14d40ee59f51dd1cbb4aff36fe72845b28a9c372
mapumarshmallow <33705819+mapumarshmallow@users.noreply.github.com>
2018-04-21 17:28:11 -0500

    Sign in page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import EventsDisplay from './EventsDisplay.js'
 import NavBar from './Components/NavBar';
+import Login from './Components/login';
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <Login />
         <EventsDisplay />
       </div>
     );
