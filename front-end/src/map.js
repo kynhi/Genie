6777c97d783aba1717cfbc15856f64cacd8c188b
Nhi Luong <nhi.luong@mavs.uta.edu>
2018-04-21 16:06:37 -0500

    map

---
+++ front-end/src/map.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export default class SimpleMap extends Component {
+  static defaultProps = {
+    center: {lat: 59.95, lng: 30.33},
+    zoom: 11
+  };
+
+  render() {
+    return (
+      <GoogleMapReact height="50px" width="50px"
+        bootstrapURLKeys={{ key: ['AIzaSyD2HroBosUVLt8KEZivUA2NfBFkzqJvmSo'] }}
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text={'Kreyser Avrora'}
+        />
+      </GoogleMapReact>
+    );
+  }
+}
