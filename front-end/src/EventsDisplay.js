14419247541f17d474f64c4839504ae70170469d
singhgagan2697 <singh.gagan2697@gmail.com>
2018-04-22 10:39:45 -0500

    added intro

--- front-end/src/EventsDisplay.js
+++ front-end/src/EventsDisplay.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import MyFancyComponent from './map.js';
+import Intro from './Intro.js';
 
 export default class EventsDisplay extends Component {
   render() {
     return (
       <div className='Map'>
-
-      <MyFancyComponent />
-
+        <Intro/>
+        <br/>
+        <MyFancyComponent />
       </div>
     );
   }
