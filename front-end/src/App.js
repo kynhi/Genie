6c18450c54f1fbbb56d7af4caf4dd6d15dfd62d8
singhgagan2697 <singh.gagan2697@gmail.com>
2018-04-22 00:56:42 -0500

    RegisterComponent value edited

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import EventsDisplay from './EventsDisplay.js'
+import EventsDisplay from './EventsDisplay.js';
 import HeaderComponent from './Components/HeaderComponent';
-import Event_Detail from './Components/Event-Detail/event-detail'
+import Event_Detail from './Components/Event-Detail/event-detail';
+import RegisterComponent from './Components/RegisterComponent';
+import $ from 'jquery';
+import './Styles/RegisterStyle.css';
 
 class App extends Component {
   render() {
@@ -10,6 +13,7 @@ class App extends Component {
         <HeaderComponent />
         <EventsDisplay />
         <Event_Detail/>
+        <RegisterComponent/>
       </div>
     );
   }
